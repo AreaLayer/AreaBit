cecdf9500f82ed5cde56422bb092ad7092f98343
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-01-18 08:56:27 -0300

    Update and rename script.h to script.rs

---
+++ contract/script.rs
@@ -0,0 +1,13 @@
+
+use bitcoin_script::bitcoin_script;
+
+
+let htlc_script = bitcoin_script! {
+    OP_IF
+        OP_SHA256 <digest> OP_EQUALVERIFY OP_DUP OP_SHA256 <seller_pubkey_hash>
+    OP_ELSE
+        100 OP_CSV OP_DROP OP_DUP OP_HASH160 <buyer_pubkey_hash>
+    OP_ENDIF
+    OP_EQUALVERIFY
+    OP_CHECKSIG
+};
