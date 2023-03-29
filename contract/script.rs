fa498a7a0f90f92763340d9954a51e5b7381efcd
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-29 06:14:24 -0300

    Update script.rs

--- contract/script.rs
+++ contract/script.rs
@@ -1,14 +1,74 @@
+use bitcoin::{Address, PublicKey, Script};
+use rgb::{fungible::Asset, Contract, ContractId, Metadata, Node, Schema, Transition};
 
-use bitcoin_script::bitcoin_script;
-use rgb::schema::mType
+// Define the loan schema
+#[derive(Schema)]
+struct LoanSchema {
+    #[rgb(field)]
+    borrower: PublicKey,
+    #[rgb(field)]
+    lender: PublicKey,
+    #[rgb(field)]
+    amount: u64,
+}
 
+// Define the loan contract
+#[derive(Contract)]
+#[rgb(schema = "LoanSchema")]
+struct LoanContract;
 
-let htlc_script = bitcoin_script! {
-    OP_IF
-        OP_SHA256 <digest> OP_EQUALVERIFY OP_DUP OP_SHA256 <seller_pubkey_hash>
-    OP_ELSE
-        100 OP_CSV OP_DROP OP_DUP OP_HASH160 <buyer_pubkey_hash>
-    OP_ENDIF
-    OP_EQUALVERIFY
-    OP_CHECKSIG
-};
+// Define the loan metadata
+#[derive(Metadata)]
+#[rgb(contract = "LoanContract")]
+struct LoanMetadata {
+    name: String,
+    ticker: String,
+    description: String,
+}
+
+fn main() {
+    // Generate borrower and lender public keys
+    let borrower_key = PublicKey::from_str("025c0176c6282f469c2ed1d15b7c0bebcf21a9e20cfaa0ab0a031157ca905de347").unwrap();
+    let lender_key = PublicKey::from_str("03b4c79970e1d1532f80d79ca5dc27c5b4703a44a7a42a85f1502db118f731d8ee").unwrap();
+
+    // Generate the loan contract
+    let loan_contract = LoanContract::new(LoanSchema {
+        borrower: borrower_key,
+        lender: lender_key,
+        amount: 100_000_000, // 1 BTC
+    });
+
+    // Generate the loan metadata
+    let loan_metadata = LoanMetadata {
+        name: "Bitcoin Loan".to_string(),
+        ticker: "LOAN".to_string(),
+        description: "A loan denominated in Bitcoin".to_string(),
+    };
+
+    // Generate the loan asset
+    let loan_asset = Asset::new(
+        ContractId::from(loan_contract),
+        Some(Metadata::from(loan_metadata)),
+    );
+
+    // Generate the borrower and lender addresses
+    let borrower_address = Address::p2wpkh(&borrower_key, bitcoin::Network::Testnet);
+    let lender_address = Address::p2wpkh(&lender_key, bitcoin::Network::Testnet);
+
+    // Fund the borrower's address with 1 BTC
+    let (utxo_txid, utxo_index) = fund_address(&borrower_address, 1_000_000_000);
+
+    // Generate the loan issuance transition
+    let issuance = loan_asset.issue(1, &loan_contract);
+
+    // Generate the loan transfer transition
+    let transfer = loan_asset.transfer(
+        1,
+        &loan_contract,
+        borrower_address.script_pubkey(),
+        lender_address.script_pubkey(),
+    );
+
+    // Generate the loan redemption transition
+    let redemption = loan_asset.rede
+
